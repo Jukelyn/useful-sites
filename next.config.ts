bd6d7d08d2dc7524439185ee8aed7665ee88c7a4
Jukelyn <viperkillergaming@gmail.com>
2025-08-13 12:46:41 -0400

    Update packages and next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: true,
+  },
+  turbopack: {},
+  devIndicators: false,
+  output: "standalone",
 };
 
 export default nextConfig;
